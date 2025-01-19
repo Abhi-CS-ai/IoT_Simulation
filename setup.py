ae5240167a2e49d1595d8b73cc36d140354e3cd5
Abhishek Gupta <abhishekg17052002@gmail.com>
2025-01-19 13:26:24 +0100

    python config

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+ from distutils.core import setup
+ setup(
+ name='{IoT_Simulation}',
+ version='1.0',
+ author='{Abhishek Gupta}',
+ license='MIT',
+ long_description=open('README.md').read(),
+ )
